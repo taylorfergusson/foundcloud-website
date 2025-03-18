f5836809453429a9a8e4abbb836352ec52331556
Taylor Fergusson <hello@taylorfergusson.com>
2025-03-18 14:25:02 -0400

    Updating to increment uploads!

--- recorder.js
+++ recorder.js
@@ -1,5 +1,7 @@
 let mediaRecorder;
 let audioChunks = [];
+let stream;
+let matchFound = false;
 
 async function checkHealth() {
     try {
@@ -19,13 +21,15 @@ async function startRecording() {
         document.getElementById("buffer").style.display = "block";
         document.getElementById("audio-status").innerText = "Loading...";
         document.getElementById("song-info").style.display = "none";
+        document.getElementById("no-matches").style.display = "none";
         document.getElementById("get-id").style.display = "none";
         // Request microphone access
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        stream = await navigator.mediaDevices.getUserMedia({ audio: true });
 
         // Set up the media recorder
         mediaRecorder = new MediaRecorder(stream);
         // mediaRecorder = new MediaRecorder(stream, { mimeType: 'audio/wav' });
+
         mediaRecorder.start();
 
         // Collect recorded audio data
@@ -33,22 +37,41 @@ async function startRecording() {
             audioChunks.push(event.data);
         };
 
-        for (let i = 1; i <= 11; i++) {
-            setTimeout(() => {
-                document.getElementById("audio-status").innerText = `Listening for ${i} seconds`;
-        
-                // Stop recording when reaching 10 seconds
-                if (i === 11) {
-                    mediaRecorder.stop();
-                }
-            }, i * 1000);
-        }
+        matchFound = false;
+        let i = 0;
+        const maxLength = 20;
+        const clipLength = 5; // 5 second clips each time
 
+        const intervalId = setInterval(() => {
+            if (matchFound) {
+                clearInterval(intervalId); // Stop the interval if matchFound is true
+                return; // Exit the interval
+            }
+        
+            document.getElementById("audio-status").innerText = `Listening for ${i + 1} seconds`;
+        
+            if (i !== 0 && i % clipLength === 0) {
+                console.log('STOPPING RECORDER IN INTERVAL ', i);
+                mediaRecorder.stop();
+                if (i < maxLength) {
+                    mediaRecorder.start();
+                } else {
+                    clearInterval(intervalId); // Stop the interval when maxLength is reached
+                    noMatches()
+                }
+            }
+
+            i++; // Increment the counter
+
+        }, 1000); // Run every second
+        
         mediaRecorder.onstop = () => {
-            stream.getTracks().forEach(track => track.stop()); // Stop mic
-            const audioBlob = new Blob(audioChunks, { type: "audio/webm" });
-            audioChunks = []; // Clear chunks
-            sendRecording(audioBlob); // Send to FastAPI
+            if (!matchFound) {
+                const audioBlob = new Blob(audioChunks, { type: "audio/webm" });
+                sendRecording(audioBlob); // Send to FastAPI
+            } else {
+                audioChunks = []; // Clear chunks
+            }
         };
 
     } catch (error) {
@@ -57,8 +80,6 @@ async function startRecording() {
 }
 
 async function sendRecording(audioBlob) {
-    document.getElementById("audio-status").innerText = "Finding match...";
-
     const formData = new FormData();
     formData.append("file", audioBlob, 'rec.webm'); // Append the file
 
@@ -71,7 +92,6 @@ async function sendRecording(audioBlob) {
         if (!response.ok) throw new Error("Upload failed");
 
         const data = await response.json(); // Get response from FastAPI
-        console.log("Server Response:", data);
         handleServerResponse(data);
     } catch (error) {
         console.error("Error uploading file:", error);
@@ -81,7 +101,12 @@ async function sendRecording(audioBlob) {
 // Function to handle the server response
 function handleServerResponse(data) {
     // Example: Display the result URL
-    if (data) {
+    if (Object.keys(data).length === 0) {
+        console.log("No matches received from server:", data);
+    } else {
+        console.log("Received data from server:", data)
+        matchFound = true;
+        stream.getTracks().forEach(track => track.stop()); // Stop mic
         document.getElementById("artwork").src = 'https://i1.sndcdn.com/artworks-' + data.artwork_path + '-t500x500.jpg';
         document.getElementById("songURL").href = 'https://soundcloud.com/' + data.song_path;
         document.getElementById("title").innerText = data.title;
@@ -90,12 +115,15 @@ function handleServerResponse(data) {
         document.getElementById("buffer").style.display = "none";
         document.getElementById("song-info").style.display = "block";
         document.getElementById("get-id").style.display = "block";
-
-    } else {
-        console.log("Unexpected response:", data);
     }
 }
 
-checkHealth()
+function noMatches() {
+    stream.getTracks().forEach(track => track.stop()); // Stop mic
+    document.getElementById("buffer").style.display = "none";
+    document.getElementById("no-matches").style.display = "block";
+    document.getElementById("get-id").style.display = "block";
+}
 
+checkHealth()
 document.getElementById("recordBtn").addEventListener("click", startRecording);
